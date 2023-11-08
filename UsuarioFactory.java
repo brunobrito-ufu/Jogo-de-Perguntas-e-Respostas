dff66194bad92ac27f636e08cbdb944ea42f22ac
joaopmarque <105685203+joaopmarque@users.noreply.github.com>
2023-11-07 22:16:37 -0300

    Delete UsuarioFactory.java

--- UsuarioFactory.java
+++
@@ -1,8 +0,0 @@
-public class UsuarioFactory {
-    public Usuario criarUsuario(String tipo, String nome, String senha) {
-        if (tipo.equalsIgnoreCase("admin")) {
-            return new Admin(nome, senha);
-        }
-        return null; // Ou lançar uma exceção se o tipo não for reconhecido
-    }
-}
