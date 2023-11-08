7f7a0da7c44b10d70976a15db68a07f66565b4ac
Bruno Brito <80076693+brunobrito-ufu@users.noreply.github.com>
2023-11-07 22:16:56 -0300

    Delete CalculadoraPontuacao.java

--- CalculadoraPontuacao.java
+++
@@ -1,5 +0,0 @@
-public interface CalculadoraPontuacao {
-
-    int CalculadoraPontuacao(int valorAtual, int valorPergunta, int respostaObtida, boolean perguntaComBonus,
-                          double tempo);
-}
